c2bbac403d671c47519e0a4fe708e0ede696cf19
zagob <matheusus1_23@hotmail.com>
2020-08-26 18:06:38 -0300

    Cadastro de usuarios

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello GoStack' })
+})
+
+app.listen(3333, () => {
+    console.log(' ✔ Server started on port 3333!' )
+})
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,47 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * Recebimento das informações
+ * Tratativa de erros/excessões
+ * Acesso ao repositório
+ */
+/** DTO */
+ interface Request {
+    provider_id: string;
+    date: Date;
+ }
+
+ /**
+  * Dependency Inversion (SOLID)
+  */
+class CreateAppointmentService {
+
+    public async execute({ date, provider_id }: Request): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+        const appointmentDate = startOfHour(date);
+
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+    
+        if (findAppointmentInSameDate) {
+            throw Error('This appointment is already booked')
+        }
+    
+        const appointment = appointmentsRepository.create({
+            provider_id,
+            date: appointmentDate,
+        });
+
+        await appointmentsRepository.save(appointment)
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
